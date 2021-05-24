bd158844be97931b534596c1eff9650cfeaace94
Pavel Koryakin <unclekoin@gmail.com>
2021-05-24 18:05:08 +0300

    feat: add header & footer

--- src/app.js
+++ src/app.js
@@ -1,23 +1,35 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath)
+
+// Setup directory for serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather App',
-    name: 'Pavel Koryakin'
+    name: 'Pavel Koryakin',
+    date: new Date().getFullYear().toString()
   })
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Me',
-    name: 'Pavel Koryakin'
+    name: 'Pavel Koryakin',
+    date: new Date().getFullYear().toString()
   })
 })
 
@@ -25,7 +37,8 @@ app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help',
     name: 'Pavel Koryakin',
-    helpText: 'This is some helpful text.'
+    helpText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto beatae ducimus error impedit perspiciatis quibusdam vel? A aliquid, dolorum fugiat illo natus obcaecati odit, perspiciatis quibusdam sint sunt suscipit!',
+    date: new Date().getFullYear().toString()
   })
 })
 
