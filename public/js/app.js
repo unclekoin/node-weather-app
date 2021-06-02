0b97fcaa054cd1eb936b4ba565866f5aa32e3e2b
Pavel Koryakin <unclekoin@gmail.com>
2021-06-02 18:51:24 +0300

    feat: finish project

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
-console.log('Client side javascript file is loaded!')
+console.log('Client side javascript file is loaded!');
+
+const content = document.getElementById('content');
+
+const weatherForm = document.getElementById('weather-form');
+const weatherInput = document.getElementById('weather-input');
+const messageOne = document.querySelector('.weather__message-one');
+const messageTwo = document.querySelector('.weather__message-two');
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const location = weatherInput.value;
+  weatherInput.value = '';
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  fetch(`http://localhost:3000/weather?address=${ location }`)
+    .then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      })
+    })
+})
+
+
--- src/app.js
+++ src/app.js
@@ -56,12 +56,12 @@ app.get('/weather', (req, res) => {
 
   geocode(address, (error, {latitude, longitude, location} = {}) => {
     if (error) {
-      return res.send({error});
+      return res.send({ error });
     }
 
     forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
-        return res.send({error});
+        return res.send({ error });
       }
       res.send({
         forecast: forecastData,
